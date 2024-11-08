f02c2f313ca81db273d27ce88cdab2ce321e8081
lithric <35087325+lithric@users.noreply.github.com>
2024-11-08 12:03:21 -0500

    Lab 9 done

--- Lab 9/code/CDList.h
+++ Lab 9/code/CDList.h
@@ -15,7 +15,8 @@ struct CDListNode {
     ListType
         datum;
     CDListNode<ListType>
-        *next;
+        *next,
+        *prev;
 };
 
 template <typename ListType>
@@ -34,6 +35,7 @@ public:
     CDList() {
 
         head = nullptr;     // as discussed in the slides
+        cur = nullptr;
         count = 0;
     }
 
@@ -48,6 +50,7 @@ public:
                 *tmp=l.head,
                 **prev = &head;
 
+            cur = nullptr;
             // count once for each node in existing list
             for (uint32_t i=0;i<l.count;i++) {
                 // make a new node
@@ -64,6 +67,16 @@ public:
 
                 // move to next node in existing list
                 tmp = tmp->next;
+            }
+
+            // point last node to first node
+            ptr->next = head;
+
+            // set all back pointers
+            prev = head;
+            for (uint32_t i=0;i<l.count;i++) {
+                ptr = ptr->next;
+                ptr->prev = prev;
             }
 
             count = l.count;
@@ -112,6 +125,7 @@ public:
 
         head = nullptr;                 // all nodes removed, reset list
         count = 0;
+        cur = nullptr;
     }
 
     //=========================================================================
@@ -134,9 +148,11 @@ public:
         CDListNode<ListType>
             *tmp = head;                // start at head of list
 
-        for (int i=0;i<count;i++) {     // step through the list
-            if (key == tmp->datum)      // if key found, stop and return index
+        for (uint32_t i=0;i<count;i++) {     // step through the list
+            if (key == tmp->datum) {     // if key found, stop and return index
+                cur = tmp;
                 return i;
+            }
             tmp = tmp->next;            // walk to next node
         }
 
@@ -160,11 +176,19 @@ public:
         CDListNode<ListType>
             *tmp = head;                // start at head of list
 
-        if (pos < 0 || pos >= count)
+        if (pos < -count || pos >= count) {
             throw std::domain_error("Invalid index");
+        }
 
-        for (int i=0;i<pos;i++)         // step through the list
-            tmp = tmp->next;            // walk to next node
+        if (pos < 0) {
+            for (int32_t i=0;i>-pos;i--) {
+                tmp = tmp->prev;
+            }
+        } else {
+            for (int32_t i=0;i<pos;i++) {        // step through the list
+                tmp = tmp->next;            // walk to next node
+            }
+        }
 
         return tmp->datum;              // return reference;
     }
@@ -206,26 +230,48 @@ public:
     void insert(uint32_t pos, const ListType &d) {
         CDListNode<ListType>
             *ptr,
-            **pred;
+            **pred,
+            *successor;
 
         // step 0: validate pos
-        if (pos > count)
+        if (pos > count) {
             throw std::domain_error("Invalid index");
+        }
 
         // step 1: allocate new node and place datum
         ptr = new CDListNode<ListType>;
         ptr->datum = d;
 
+        if (count == 0) {
+            // make the node point to itself forwards and backwards
+            ptr->next = ptr->prev = ptr;
+
+            // set the head pointer
+            head = ptr;
+
+            // update the count
+            count++;
+
+            // done
+            return;
+        }
+
         // step 2: find the predecessor
         pred = &head;
         for (int i=0;i<pos;i++)
             pred = &((*pred)->next);	// walk to next pointer
+        // find the node after the new node
+        successor = *pred;
 
         // step 3: copy predecessor's next pointer to new node
         ptr->next = *pred;
+        // new node points backwards to predecessor
+        ptr->prev = successor->prev;
 
         // step 4: predecessor points to new node
         *pred = ptr;
+        // successor points to new node
+        successor->prev = ptr;
 
         // step 5: update count
         count++;
@@ -249,11 +295,26 @@ public:
     void remove(uint32_t pos) {
         CDListNode<ListType>
             *ptr,
-            **pred;
+            **pred,
+            *successor;
 
         // step 0: validate pos
-        if (pos >= count)
+        if (pos >= count) {
             throw std::domain_error("Invalid index");
+        }
+        if (count==1) {
+            // remove the node
+            delete head;
+
+            // reset head and cur
+            head = cur = nullptr;
+
+            // update the count
+            count--;
+
+            // done
+            return;
+        }
 
         // step 1: find the predecessor
         pred = &head;
@@ -262,9 +323,17 @@ public:
 
         // step 2: remember node to be deleted
         ptr = *pred;
+        // find the node after the node to be deleted
+        successor = ptr->next;
+        // if we are removing the current node, set cur to nullptr
+        if (cur == ptr) {
+            cur = nullptr;
+        }
 
         // step 3: copy ntbd's pointer to predecessor... pointing around ntbd
         *pred = ptr->next;
+        // successor points backwards to predecessor
+        successor->prev = ptr->prev;
 
         // step 4: delete the node
         delete ptr;
@@ -273,12 +342,61 @@ public:
         count--;
     }
 
+    ListType& first() {
+        if (count==0) {
+            throw std::runtime_error("tried to access first of empty list.");
+        }
+        cur = head;
+        return cur->datum;
+    }
+
+    ListType& last() {
+        if (count==0) {
+            throw std::runtime_error("tried to access last of empty list.");
+        }
+        cur = head->prev;
+        return cur->datum;
+    }
+
+    bool isFirst() {
+        if (cur==nullptr) return false;
+        return cur==head;
+    }
+
+    bool isLast() {
+        if (cur==nullptr) return false;
+        return cur==head->prev;
+    }
+
+    ListType& next() {
+        if (cur==nullptr) {
+            throw std::runtime_error("tried to access next of empty list.");
+        }
+        cur = cur->next;
+        return cur->datum;
+    }
+
+    ListType& prev() {
+        if (cur==nullptr) {
+            throw std::runtime_error("tried to access prev of empty list.");
+        }
+        cur = cur->prev;
+        return cur->datum;
+    }
+
+    ListType& current() {
+        if (cur==nullptr) {
+            throw std::runtime_error("tried to access current of empty list.");
+        }
+        return cur->datum;
+    }
+
 private:
     CDListNode<ListType>
-        *head;
-    uint32_t
+        *head,
+        *cur;
+    int32_t
         count;
-
 };
 
 #endif //LINEARLIST_H
